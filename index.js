9672864cc68e1306b8f0af6dd8cd452e2ad6f219
Michel <42797392+Misk77@users.noreply.github.com>
2020-10-20 20:38:02 +0200

    Update index.js
    test webhook openshift

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! FROM Misk77 REPO docker-demo');
+  res.send('Hello World! FROM Misk77 REPO docker-demo. ');
 });
 
 var server = app.listen(3000, function () {
